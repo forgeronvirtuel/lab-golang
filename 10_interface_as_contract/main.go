e827a2fe9432be9a911599be22a19ffbe431a6e2
forgeronvirtuel <forgeronvirtuel@gmail.com>
2022-09-19 17:20:47 +0200

    post: interface as contract

---
+++ 10_interface_as_contract/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "io"
+
+// analyze computes the relevance rate for each keyword in
+// relation to the text extracted from the io.Reader object.
+func analyze(keywords []string, r io.Reader) map[string]float64 {
+	// Do stuff
+}
