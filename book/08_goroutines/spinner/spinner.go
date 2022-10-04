d4fe8f352b0fc36c98f71a08e8eda879aefe47e4
forgeronvirtuel <forgeronvirtuel@gmail.com>
2022-10-04 09:21:17 +0200

    feat: spinner

---
+++ book/08_goroutines/spinner/spinner.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	go spinner(100 * time.Millisecond)
+	const n = 45
+	fmt.Printf("\rFibonacci(%d) = %d\n", n, fib(n))
+}
+
+func spinner(delay time.Duration) {
+	for {
+		for _, r := range `|/-\` {
+			fmt.Printf("\r%c", r)
+			time.Sleep(delay)
+		}
+	}
+}
+
+func fib(n int) int {
+	if n < 2 {
+		return n
+	}
+	return fib(n-1) + fib(n-2)
+}
