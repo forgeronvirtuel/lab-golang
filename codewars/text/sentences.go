f6c4d3d986652e7deea43ece498bf9eb6be5305b
forgeronvirtuel <forgeronvirtuel@gmail.com>
2022-10-05 21:00:52 +0200

    feat: full implementation to reverse a string

--- codewars/text/sentences.go
+++ codewars/text/sentences.go
@@ -26,25 +26,31 @@ func ReverseWords(txt string) string {
 }
 
 // Full implementation
-//func ReverseWords(txt string) string {
-//	// Part 1. split the string
-//	previ := -1
-//	var strs []string
-//	for i := 0; i < len(txt); i++ {
-//		if rune(txt[i]) == ' ' {
-//			strs = append(strs, txt[previ+1:i])
-//			previ = i
-//		}
-//	}
-//	strs = append(strs, txt[previ+1:len(txt)])
-//
-//	// Part 2. reverse order
-//	length := len(strs)
-//	for i, j := 0, length-1; i < length/2; i, j = i+1, j-1 {
-//		strs[i], strs[j] = strs[j], strs[i]
-//	}
-//
-//
-//	// Part 3. Recreate the string
-//	return strings.Join(strs, " ")
-//}
+func ReverseWordsFull(txt string) string {
+	// Part 1. split the string
+	previ := -1
+	var strs []string
+	for i := 0; i < len(txt); i++ {
+		if rune(txt[i]) == ' ' {
+			strs = append(strs, txt[previ+1:i])
+			previ = i
+		}
+	}
+	strs = append(strs, txt[previ+1:])
+
+	// Part 2. reverse order
+	length := len(strs)
+	for i, j := 0, length-1; i < length/2; i, j = i+1, j-1 {
+		strs[i], strs[j] = strs[j], strs[i]
+	}
+
+	// Part 3. Recreate the string
+	var b strings.Builder
+	lastIdx := len(strs) - 1
+	for _, s := range strs[:lastIdx] {
+		b.WriteString(s)
+		b.WriteRune(' ')
+	}
+	b.WriteString(strs[lastIdx])
+	return b.String()
+}
