8e0e4c86c4faeeae64fb1650aaefe9e3e562fb2b
forgeronvirtuel <forgeronvirtuel@gmail.com>
2022-10-15 15:32:55 +0200

    example: leak goroutine with memory check

--- book/08_goroutines/leak/leakexample.go
+++ book/08_goroutines/leak/leakexample.go
@@ -3,16 +3,13 @@ package main
 import (
 	"bytes"
 	"crypto/rand"
+	"fmt"
+	"runtime"
+	"time"
 )
 
 const nbConsumer = 10
 const nbMessage = 20
-
-func getQuery() []byte {
-	data := make([]byte, 1024)
-	rand.Read(data)
-	return data
-}
 
 func queryChecker(packets [][]byte, word []byte) bool {
 	input := make(chan []byte, nbMessage)
@@ -22,9 +19,8 @@ func queryChecker(packets [][]byte, word []byte) bool {
 	for i := 0; i < nbConsumer; i++ {
 		go func(n int, input <-chan []byte, res chan<- bool) {
 			data := <-input
-
 			// Check the query
-			res <- bytes.Compare(data, word) == 0
+			res <- bytes.Contains(data, word)
 		}(i, input, res)
 	}
 
@@ -34,11 +30,37 @@ func queryChecker(packets [][]byte, word []byte) bool {
 	}
 
 	// stop at the first wrong result
-	for ok := range res {
-		if !ok {
+	for i := 0; i < nbConsumer; i++ {
+		if !(<-res) {
 			return false // all unfinished goroutine will be locked
 		}
 	}
-
 	return true
+}
+
+const MBdiv = 1024.0 * 1024.0
+const nbIter = 1_000
+
+func main() {
+	m := runtime.MemStats{}
+
+	for i := 0; i < nbIter; i++ {
+		packets := make([][]byte, nbMessage)
+		for i, _ := range packets {
+			data := make([]byte, 1024)
+			rand.Read(data)
+			word := []byte("HTTP")
+			copy(data[0:len(word)], word)
+			packets[i] = data
+		}
+		if ok := queryChecker(packets, []byte("HTTP")); !ok {
+			fmt.Printf("[nok] ")
+		} else {
+			fmt.Printf("[ ok] ")
+		}
+
+		runtime.ReadMemStats(&m)
+		fmt.Printf("HeapAlloc: %0.2f MB\r", float64(m.HeapAlloc)/MBdiv)
+		time.Sleep(5 * time.Microsecond)
+	}
 }
