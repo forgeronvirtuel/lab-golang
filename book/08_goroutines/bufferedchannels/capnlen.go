3efad84e60dd77bd61fa55c0d28051e9d9bd2767
forgeronvirtuel <forgeronvirtuel@gmail.com>
2022-10-16 11:46:12 +0200

    example: using len in a concurrent context

--- book/08_goroutines/bufferedchannels/capnlen.go
+++ book/08_goroutines/bufferedchannels/capnlen.go
@@ -1,16 +1,17 @@
 package main
 
+const nbElement = 100
+
 func main() {
-	ch := make(chan int, 10)
+	ch := make(chan int, nbElement*2)
 	go func() {
-		for i := 0; i < 30; i++ {
+		for i := 0; i < nbElement; i++ {
 			// (1) Here is the problem, let say that
-			// this goroutine reads the len of ch
-			// and it indicate 1, so this condition
+			// this goroutine reads the len of ch,
+			// and it indicates 0, so this condition
 			// pass.
-			if len(ch) > 0 {
-				// (2) But just after the call to len(ch)
-				// this goroutine sleep.
+			if len(ch)%2 == 0 {
+				// (2) But just after this goroutine sleep.
 
 				// (4) Now this goroutine wakes up, but
 				// the condition is no more valid.
@@ -20,11 +21,16 @@ func main() {
 	}()
 
 	go func() {
-		for i := 0; i < 200; i++ {
-			// (3) Now this groutine wakes up. It will add
+		for i := 0; i < nbElement; i++ {
+			// (3) Now this goroutine wakes up. It will add
 			// one element to the channel and return to sleep
 			// while trying to add the next one.
 			ch <- i
 		}
 	}()
+
+	// (5) The channel is read by the main goroutine
+	for i := 0; i < nbElement*2; i++ {
+		<-ch
+	}
 }
