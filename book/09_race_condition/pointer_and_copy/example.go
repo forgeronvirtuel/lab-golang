92ed044ad887c1918540df5e7920bff92c056599
forgeronvirtuel <forgeronvirtuel@gmail.com>
2022-11-21 21:33:25 +0100

    example: struct and copy pointer

---
+++ book/09_race_condition/pointer_and_copy/example.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type RPC struct {
+	result int
+	done   chan struct{}
+}
+
+func (rpc *RPC) compute() {
+	time.Sleep(time.Second) // strenuous computation intensifies
+	rpc.result = 42
+	close(rpc.done)
+}
+
+func (rpc RPC) version() int {
+	<-rpc.done
+	fmt.Println("version done")
+	return 1 // never going to need to change this
+}
+
+func main() {
+	rpc := &RPC{done: make(chan struct{})}
+
+	go rpc.compute()         // kick off computation in the background
+	version := rpc.version() // grab some other information while we're waiting
+	fmt.Println("main done")
+	<-rpc.done // wait for computation to finish
+	result := rpc.result
+	fmt.Printf("RPC computation complete, result: %d, version: %d\n", result, version)
+}
